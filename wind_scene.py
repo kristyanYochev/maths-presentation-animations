426e35d2ca651d6442ca529affa774b3837e78fe
Kristyan Yochev <kris.yochev@gmail.com>
2019-06-20 21:26:28 +0300

    Init project

---
+++ wind_scene.py
@@ -0,0 +1,14 @@
+from manimlib.imports import *
+import os
+import pyclbr
+
+
+class MainScene(Scene):
+    def construct(self):
+        v = Vector(2.9 * UP)
+        nm_text = TexMobject("N_{M}")
+        nm_text.shift(3.1 * UP)
+
+        self.play(Write(nm_text))
+        self.play(ShowCreation(v))
+        
