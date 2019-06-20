eeb8d5115cbe2fb23de2ae8d0d80537542bebbbc
Kristyan Yochev <kris.yochev@gmail.com>
2019-06-20 22:46:19 +0300

    [Update] A bit more working

--- wind_scene.py
+++ wind_scene.py
@@ -5,10 +5,42 @@ import pyclbr
 
 class WindScene(Scene):
     def construct(self):
-        v = Vector(2.9 * UP)
-        nm_text = TexMobject("N_{M}")
-        nm_text.shift(3.1 * UP)
+        magentic_north = Vector(3 * UP)
+        magentic_north_label = TexMobject("N_{M}")
+        magentic_north_label.move_to(magentic_north)
 
-        self.play(Write(nm_text))
-        self.play(ShowCreation(v))
-        
+        self.play(ShowCreation(magentic_north))
+        self.play(Write(magentic_north_label))
+
+        gs_tip = 3.5 * RIGHT
+        gs = Vector(gs_tip)
+        gs_label = TexMobject("GS")
+        gs_label.move_to(gs)
+
+        self.play(ShowCreation(gs))
+        self.play(Write(gs_label))
+
+        tas_tip = normalize(np.array([1.5, 1, 0])) * 3
+        tas = Vector(tas_tip)
+        tas_label = TexMobject("TAS")
+        tas_label.move_to(tas)
+
+        self.play(ShowCreation(tas))
+        self.play(Write(tas_label))
+
+        wind_tip = 3.5 * RIGHT - tas_tip
+        wind = Vector(wind_tip)
+        wind_label = TexMobject("W/V")
+        wind_label.move_to(wind)  
+
+        self.play(ShowCreation(wind))
+        self.play(Write(wind_label))
+        self.wait()
+
+        new_wind = Vector(wind_tip)
+        new_wind.shift(tas_tip)
+        new_wind_label = TexMobject("W/V")
+        new_wind_label.move_to(new_wind)
+
+        self.play(Transform(wind, new_wind))
+        self.play(Transform(wind_label, new_wind_label))
